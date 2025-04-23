b01c1c1b2e5d4ac7b3621a4c587987e4ad196329
pshvydkyi-clgx <pshvydkyi@corelogic.com>
2025-04-23 11:08:20 +0300

    Task 1_4

---
+++ MultiThreading.Task4.Threads.Join/VariantA.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreading.Task4.Threads.Join
+{
+    public static class VariantA
+    {
+        static int recursionCount = 0;
+        static int _result = 0;
+
+        static public void Execute() {
+            Console.WriteLine("VARIANT A");
+            Console.WriteLine();
+
+            recursionCount = 10;
+
+            Random rnd = new Random();
+            _result = rnd.Next(1, 255);
+            RunThreads(_result);
+
+        }
+
+        static void RunThreads(int data)
+        {
+            recursionCount--;
+            Thread thread = new Thread(decrement);
+            thread.Start(data);
+            thread.Join();
+            Console.WriteLine($"recursive level {10 - recursionCount} value is {data}");
+
+            if (recursionCount > 0)
+            {
+                RunThreads(_result);
+            }
+            return;
+        }
+
+        static void decrement(object state)
+        {
+            int number = (int)state;
+            number--;
+            _result = number;
+        }
+    }
+}
---
+++ MultiThreading.Task4.Threads.Join/VariantB.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreading.Task4.Threads.Join
+{
+    public static class VariantB
+    {
+        static int recursionCount = 0;
+        static int _result = 0;
+        static Semaphore semaphore = new Semaphore(0, 1);
+
+
+        public static void Execute()
+        {
+            Console.WriteLine("VARIANT B");
+            Console.WriteLine();
+            recursionCount = 10;
+
+            Random rnd = new Random();
+            _result = rnd.Next(1, 255);
+            RunThreads(_result);
+
+        }
+
+        static void RunThreads(int data)
+        {
+            recursionCount--;
+            Console.WriteLine($"recursive level {10 - recursionCount} value is {data}");
+            ThreadPool.QueueUserWorkItem(decrement, data);
+            semaphore.WaitOne();
+            if (recursionCount > 0)
+            {
+                RunThreads(_result);
+            }
+            return;
+        }
+
+        static void decrement(object state)
+        {
+            int number = (int)state;
+            number--;
+            _result = number;
+            semaphore.Release();
+        }
+    }
+}
